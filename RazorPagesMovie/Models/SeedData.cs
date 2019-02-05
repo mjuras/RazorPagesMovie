44d39c2181f782c11ca8863e028c48c258d082f6
mark.juras <mark@greatmigrations.com>
2019-02-04 20:56:27 -0500

    Update seed data do include rating

--- RazorPagesMovie/Models/SeedData.cs
+++ RazorPagesMovie/Models/SeedData.cs
@@ -20,37 +20,10 @@ namespace RazorPagesMovie.Models
                 }
 
                 context.Movie.AddRange(
-                    new Movie
-                    {
-                        Title = "When Harry Met Sally",
-                        ReleaseDate = DateTime.Parse("1989-2-12"),
-                        Genre = "Romantic Comedy",
-                        Price = 7.99M
-                    },
-
-                    new Movie
-                    {
-                        Title = "Ghostbusters ",
-                        ReleaseDate = DateTime.Parse("1984-3-13"),
-                        Genre = "Comedy",
-                        Price = 8.99M
-                    },
-
-                    new Movie
-                    {
-                        Title = "Ghostbusters 2",
-                        ReleaseDate = DateTime.Parse("1986-2-23"),
-                        Genre = "Comedy",
-                        Price = 9.99M
-                    },
-
-                    new Movie
-                    {
-                        Title = "Rio Bravo",
-                        ReleaseDate = DateTime.Parse("1959-4-15"),
-                        Genre = "Western",
-                        Price = 3.99M
-                    }
+                    new Movie {Title = "When Harry Met Sally",ReleaseDate = DateTime.Parse("1989-2-12"),Genre = "Romantic Comedy",Price = 7.99M,Rating = "PG"},
+                    new Movie {Title = "Ghostbusters"        ,ReleaseDate = DateTime.Parse("1984-3-13"),Genre = "Comedy"         ,Price = 8.99M,Rating = "PG"},
+                    new Movie {Title = "Ghostbusters 2"      ,ReleaseDate = DateTime.Parse("1986-2-23"),Genre = "Comedy"         ,Price = 9.99M,Rating = "PG"},
+                    new Movie {Title = "Rio Bravo"           ,ReleaseDate = DateTime.Parse("1959-4-15"),Genre = "Western"        ,Price = 3.99M,Rating = "PG"}
                 );
                 context.SaveChanges();
             }
